14add6636436442ecac9a16d4d3ca585815981fd
Welingson Expedito dos Santos <38848597+Welingson@users.noreply.github.com>
2020-04-18 23:12:54 -0300

    Update scripts.js

--- themes/prefeitura/site/assets/js/scripts.js
+++ themes/prefeitura/site/assets/js/scripts.js
@@ -1,37 +1,11 @@
-let menu = document.getElementsByClassName('menu');
-let screen = window.matchMedia('(min-width: 992px) and (max-width: 1195px)');
+// collpase
+$(".document_collapse").click(function () {
+    var collapse = $(this);
 
-screen.matches ? menu[0].classList.remove("navbar-expand-lg"): ""; 
-
-
-function popUpFacebook(){
-
-    let url = "https://www.facebook.com/sharer/sharer.php?u=https://www.wetechnologia.com.br/brazopolis/investimento-em-tecnologia.html&amp;src=sdkpreparse";
-
-    window.open(url, "", "popup");
-
-}
-
-
-function popUpTwitter() {
-
-    let url = "https://twitter.com/intent/tweet?text=Prefeitura&url=https://wetechnologia.com.br/brazopolis/investimento-em-tecnologia.html";
-
-    window.open(url, "", "popup");
-
-}
-
-function popUpLinkedin() {
-    let url = "https://www.linkedin.com/sharing/share-offsite/?url=https://wetechnologia.com.br/brazopolis/investimento-em-tecnologia.html";
-    window.open(url, "", "popup");
-
-}
-
-function popUpWhatsApp(){
-    let url = "https://api.whatsapp.com/send?text=https://wetechnologia.com.br/brazopolis/investimento-em-tecnologia.html";
-    window.open(url, "", "popup");
-}
-
-
-
-
+    if (collapse.find(".document_collapse_box").is(":visible")) {
+        collapse.find(".document_collapse_box").slideUp(200);
+    } else {
+        collapse.parent().find(".document_collapse_box").slideUp(200);
+        collapse.find(".document_collapse_box").slideDown(200);
+    }
+});
