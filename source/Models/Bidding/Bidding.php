4ff3cf67c71b162e5d4d6afc0754331a689d9e73
Welingson Expedito dos Santos <38848597+Welingson@users.noreply.github.com>
2020-04-18 22:57:54 -0300

    Update Bidding.php

--- source/Models/Bidding/Bidding.php
+++ source/Models/Bidding/Bidding.php
@@ -14,13 +14,27 @@ use Source\Models\Category;
 class Bidding extends Model
 {
     protected $all;
+ 
 
     /**
      * Bidding constructor.
      */
     public function __construct()
     {
-        parent::__construct("tb_bidding", ["id"], ["process_number", "object", "bidding_number", "modality", "status"]);
+    
+
+        parent::__construct(
+            "tb_bidding", 
+            ["id"], 
+            ["process_number", "object", "bidding_number", "modality", "status"],
+            [
+                "process_number" => "process_number = :process_number",
+                "search" => "MATCH(object) AGAINST(:search)",
+                "modality" => "modality = :modality",
+                "status" => "status = :status"
+                
+            ]
+        );
     }
 
     /**
@@ -39,73 +53,19 @@ class Bidding extends Model
     }
 
 
+    /**
+     * @param string $idBidding
+     * @return Array|null
+     */
     public function findAttachment(string $idBidding): ?Array
     {
         if ($idBidding){
-            return (new Attachment())->find("id_bidding=:id_bidding", "id_bidding={$idBidding}")->fetch(true);
+            return (new Attachment())->find("id_bidding=:id_bidding", "id_bidding={$idBidding}")->order("date_pub DESC")->fetch(true);
         }
 
         return null;
 
     }
-
-//     public function filterSearch(array $data)
-//     {
-//         var_dump($data);
-//         $filter = [
-//             "process_number" => "process_number = :process_number",
-//             "search" => "MATCH(object) AGAINST(:search)",
-//             "status" => "status = :status",
-//             "modality" => "modality = :modality"
-//         ];
-
-//         $paramns = null;
-
-//         foreach ($filter as $key => $value) {
-//             if (array_key_exists($key, $data)) {
-//                 if ($data[$key] != "all") {
-//                     $paramns[$key] = "{$key}={$data[$key]}";
-//                 } else {
-//                     unset($filter[$key]);
-//                 }
-//             } else {
-//                 unset($filter[$key]);
-//             }
-//         }
-
-//         if (!empty($data["dateOne"]) && isset($data["dateOne"]) && $data["dateOne"] != "all" &&
-//             !empty($data["dateTwo"]) && isset($data["dateTwo"]) && $data["dateTwo"] != "all") {
-//             $filter["date"] = "created_at BETWEEN :dateOne AND :dateTwo";
-//             $data["date"] = "";
-
-//             $paramns["dateOne"] = "dateOne=" . $data["dateOne"];
-//             $paramns["dateTwo"] = "dateTwo=" . $data["dateTwo"];
-
-//             $dateOne = new \DateTime($data["dateOne"]);
-//             $dateTwo = new \DateTime($data["dateTwo"]);
-
-//             if (($dateOne->diff($dateTwo))->invert == 1) {
-//                 $paramns["dateOne"] = "dateOne=" . $data["dateTwo"];
-//                 $paramns["dateTwo"] = "dateTwo=" . $data["dateOne"];
-//             }
-//         } elseif (!empty($data["dateOne"]) && isset($data["dateOne"]) && $data["dateOne"] != "all") {
-//             $filter["date"] = "created_at >= :date";
-//             $data["date"] = "";
-//             $paramns["date"] = "date=" . $data["dateOne"];
-//         } elseif (!empty($data["dateTwo"]) && isset($data["dateTwo"]) && $data["dateTwo"] != "all") {
-//             $filter["date"] = "created_at >= :date";
-//             $data["date"] = "";
-//             $paramns["date"] = "date=" . $data["dateTwo"];
-//         }
-
-//         $filter = implode(" AND ", $filter);
-//         $paramns = ($paramns)? implode("&", $paramns):"";
-
-// //        return (new Bidding())->find($filter, $paramns)->order("created_at DESC");
-
-
-//         return (new Bidding())->find($filter, $paramns)->order("created_at DESC")->fetch(true);
-//     }
 
 
     /**
@@ -122,13 +82,18 @@ class Bidding extends Model
 
     }
 
+
+    /**
+     * @param string $modalityId
+     * @return Model|null
+     */
     public function findByModality(string $modalityId):?Model
     {
         if (!$modalityId){
             return null;
         }
 
-        $bidding = parent::find("modality=:modality ORDER BY created_at DESC", "modality={$modalityId}");
+        $bidding = parent::find("modality=:modality", "modality={$modalityId}");
 
         return $bidding;
 
@@ -147,8 +112,5 @@ class Bidding extends Model
         return null;
 
     }
-
-
-
 
 }
